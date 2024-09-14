c6785201ec0237d6acc1aa43b2883e70fd8d854c
Ahmet Burak Çiçek <cck181851@gmail.com>
2024-09-14 14:03:16 +0300

    Update leetcode-python.py

--- leetcode-python.py
+++ leetcode-python.py
@@ -39,3 +39,49 @@ class Solution:
             
         return res
             
+"""
+2458. Height of Binary Tree After Subtree Removal Queries
+
+You are given the root of a binary tree with n nodes. Each node is assigned a unique value from 1 to n. You are also given an array queries of size m.
+
+You have to perform m independent queries on the tree where in the ith query you do the following:
+
+Remove the subtree rooted at the node with the value queries[i] from the tree. It is guaranteed that queries[i] will not be equal to the value of the root.
+Return an array answer of size m where answer[i] is the height of the tree after performing the ith query.
+
+Note:
+
+The queries are independent, so the tree returns to its initial state after each query.
+The height of a tree is the number of edges in the longest simple path from the root to some node in the tree
+"""
+
+class Solution:
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        d=defaultdict(list)
+        level_map={}
+        
+        def f(node,level):
+            if not node:
+                return 0
+            mx=1
+            level_map[node.val]=level
+            for child in [node.left,node.right]:
+                if child is None:continue
+                h=f(child,level+1)
+                d[level].append([node.val,child.val,h+1])
+                mx=max(mx,h+1)
+            d[level]=sorted(d[level],key=lambda x:-x[2])[:3]
+            return mx
+        
+        f(root,0)
+        res=[]
+        for q in queries:
+            level=level_map[q]-1
+            mx=-1
+            for node,child,h in d[level]:
+                if child==q:continue
+                mx=max(mx,h)
+            res+=[max(level,mx+level-1)]
+        return res
+                
+            
